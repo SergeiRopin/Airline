d6a0ca4ee95f9e0948600bbc61c94de9279f2316
Sergei <ropin.sergei@gmail.com>
2016-08-16 02:06:00 +0300

    Added classes

---
+++ Airline/Passenger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airline
+{
+    class Passenger
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Nationality { get; private set; }
+        public string Passport { get; private set; }
+        public string Birthday { get; private set; }
+        public Sex Sex { get; private set; }
+        public Ticket Ticket { get; private set; }
+
+        //Ticket ticket = new Ticket();
+        public Passenger(string firstName, string lastName, string nationality, string passport, string birthday,
+            Sex sex, Ticket ticket)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Nationality = nationality;
+            Passport = passport;
+            Birthday = birthday;
+            Sex = sex;
+            Ticket = ticket;
+        }
+    }
+}
---
+++ Airline/Ticket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airline
+{
+    class Ticket
+    {
+        public FlightClass FlightClass { get; private set; }
+        public double Price { get; private set; }
+
+        public Ticket(FlightClass flightClass, double price)
+        {
+            FlightClass = flightClass;
+            Price = price;
+        }
+
+        public Ticket() { }
+    }
+}
